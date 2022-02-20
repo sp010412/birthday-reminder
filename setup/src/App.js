957c317056324b6933b8e80988a945c366c1e284
sp010412 <smajwede7gmail.com>
2022-02-20 14:57:32 +0200

    button view

--- setup/src/App.js
+++ setup/src/App.js
@@ -3,15 +3,20 @@ import data from './data';
 import List from './List';
 function App() {
 
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data);
+  const list = () => {
+    setPeople(data);
+  };
 
   return (
     <main>
       <section className="container">
         <h3> Birthdays Today</h3>
         <List people={people} />
+       
+        <button onClick={list}> view</button>
         <button onClick={() => setPeople([])}> clear all</button>
-    
+
       </section>
     </main>
   );
